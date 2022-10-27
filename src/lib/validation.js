c977e288660ff713986caaf67fb8a36c6622a46f
jameshsu1125 <jameshsu1125@gmail.com>
2022-10-27 13:52:31 +0800

    update

---
+++ src/lib/validation.js
@@ -0,0 +1,7 @@
+const email = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+const number = /^\d+\.?\d*$/;
+
+export { email, number };
+const Validation = { email, number };
+
+export default Validation;
